dbfa5b6a9a1065a74efe19ebc10b00e657aa6356
Draven Nova <draven77medina@hotmail.com>
2026-02-26 10:06:46 -0800

    Finish implementation of user negative count & matrix

--- Assignment_2/array3.cpp
+++ Assignment_2/array3.cpp
@@ -8,19 +8,45 @@ int main()
     cout << "Enter the size of the 2D array: ";
     cin >> arraySize;
 
-    if(arraySize < 1 || arraySize > 10)
+    if(arraySize < 1)
     {
-        cout << "ERROR: your array is too large! Enter 1 to 10." << endl;
+        cout << "Error: You entered an incorrect value for the array size!" << endl;
+        return 1;
     }
-    vector<int> array[arraySize];
-
+    if(arraySize > 10)
+    {
+        cout << "ERROR: Your array is too large! Enter 1 to 10.";
+        return 1;
+    }
+    
+    int col = arraySize;
+    int array[arraySize][col];
     for(int i = 0 ; i < arraySize; i++)
     {
-        for(int j = 0; j < arraySize; j++)
+        cout << "Enter the values in the array for row " << i + 1 << ", seperated by a space , and press enter: ";
+        for(int j = 0; j < col; j++)
         {
-            cout << "Enter the values in the array for row " << i + 1 << ", seperated by a space , and press enter: ";
             cin >> array[i][j];
         }
     }
-    
+    int negativeCount = 0;
+    for(int i = 0; i < arraySize; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            if(array[i][j] < 0)
+            {
+                negativeCount++;
+            }
+        }
+    }
+    if(negativeCount >= 1)
+    {
+        cout << "There are " << negativeCount << " negative values!" << endl;
+    }
+    else
+    {
+        cout << "All values are non-negative!" << endl;
+    }
+
 }
