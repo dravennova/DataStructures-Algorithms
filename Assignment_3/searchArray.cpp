33342e37f2508a94a30f9fbc485d7072bf695c9d
Draven Nova <draven77medina@hotmail.com>
2026-03-02 23:45:34 -0800

    error on return, need to fix logic

--- Assignment_3/searchArray.cpp
+++ Assignment_3/searchArray.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+    int arraySize;
+
+    cout << "Enter the size of the array: ";
+    cin >> arraySize;
+
+    if(arraySize <= 0)
+    {
+        cout << "Error: You entered an incorrect value for the array size!" << endl;
+        return 1;
+    }
+
+
+    cout << "Enter the numbers in the array, seperated by a space, and press enter: ";
+    int* array = new int[arraySize];
+    for(int i = 0; i < arraySize; i++)
+    {
+        cin >> array[i];
+    }
     
+    int v;
+    cout << "Enter a number to search for in the array: ";
+    cin >> v;
+
+    for(int i = 0; i < arraySize; i++)
+    {
+        if(array[i] == v)
+        {
+            cout << "We found value " << v << " at index " << array[i] << " which took " << i << " checks" << endl;
+        }
+        if(i == 1)
+        {
+            cout << "We ran the best case scenario!" << endl;
+        }
+        if(array[i] < i)
+        {
+            cout << "We ran into the worst case scenario!" << endl;
+        }
+    }
 }
