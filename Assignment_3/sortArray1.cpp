f53b3b888b5f023f70278db13c3d005051422425
Draven Nova <draven77medina@hotmail.com>
2026-03-05 21:47:26 -0800

    added insertion sort arrray

--- Assignment_3/sortArray1.cpp
+++ Assignment_3/sortArray1.cpp
@@ -14,22 +14,29 @@ int main()
         return 1;
     }
     
-    cout << "Enter the numbers in the array, seperatd by a space, and press enter: " << endl;
-
+    cout << "Enter the numbers in the array, seperatd by a space, and press enter: ";
     int* array = new int[arraySize];
     for(int i = 0; i < arraySize; i++)
     {
         cin >> array[i];
     }
 
-    for(int i = 0; i < arraySize; i++)
+    for(int i = 1; i < arraySize; i++)
     {
-        int Max = array[0];
-        int iMax = 0;
-        for(int j = 1; j < i; j++)
+        int key = array[i];
+        int j = i - 1;
+
+        while(j >= 0 && key < array[j])
         {
-            
+            array[j+1] = array[j];
+            --j;
         }
+        array[j+1] = key;
+    }
+
+    for(int k = 0; k < arraySize; k++)
+    {
+        cout << array[k] << " ";
     }
 
 }
